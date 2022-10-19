1906c99a4ad04e1edc9a5e7a369dbdc59b91d36c
Kondin Dmitriy <dkondin@gmail.com>
2022-10-19 10:35:46 +0700

    загружаем value_string для SelectByQuery

--- src/Eloquent/ValueObjects/SelectByQuery.php
+++ src/Eloquent/ValueObjects/SelectByQuery.php
@@ -3,6 +3,7 @@
 namespace Sitebill\Dragon\Eloquent\ValueObjects;
 
 use Sitebill\Dragon\Eloquent\Column;
+use Sitebill\Dragon\Helpers\DragonHelper;
 
 class SelectByQuery extends BaseValueObject
 {
@@ -16,6 +17,15 @@ class SelectByQuery extends BaseValueObject
     public function __construct( $model, $key, $value, $attributes, Column $column = null )
     {
         parent::__construct($model, $key, $value, $attributes, $column);
+        $this->value_string = $this->getValueStringByKey($model, $key, $value, $attributes, $column);
+    }
+
+    public function getValueStringByKey ( $model, $key, $value, $attributes, Column $column) {
+        $dataModel = DragonHelper::getDynamicModel($column->primary_key_table);
+        $records = $dataModel->pluck($column->value_name, $column->primary_key_name)->toArray();
+        if ( $records and isset($records[$value]) ) {
+            return $records[$value];
+        }
     }
 
     public function __toString () {
